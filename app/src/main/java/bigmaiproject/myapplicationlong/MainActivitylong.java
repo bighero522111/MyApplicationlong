93a9296ef9ee0a9c3ab9d1f8f18f7df7f5dde1bd
meteewat <bighero522111@gmail.com>
2017-07-27 21:40:11 +0700

    Initial commit

---
+++ app/src/main/java/bigmaiproject/myapplicationlong/MainActivitylong.java
@@ -0,0 +1,13 @@
+package bigmaiproject.myapplicationlong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitylong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activitylong);
+    }
+}
